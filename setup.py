65400b71ab14dd72382d57ebfe78b2d5bdd3abea
Matthew Carlson <macarl@microsoft.com>
2020-02-27 09:50:54 -0800

    inital import of pytools

---
+++ setup.py
@@ -0,0 +1,62 @@
+## @file setup.py
+# This contains setup info for edk2-pytool-library pip module
+#
+##
+# Copyright (c) Microsoft Corporation
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+##
+
+import setuptools
+from setuptools.command.sdist import sdist
+from setuptools.command.install import install
+from setuptools.command.develop import develop
+from edk2toollib.windows.locate_tools import _DownloadVsWhere
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
+
+class PostSdistCommand(sdist):
+    """Post-sdist."""
+    def run(self):
+        sdist.run(self)
+
+
+class PostInstallCommand(install):
+    """Post-install."""
+    def run(self):
+        install.run(self)
+
+
+class PostDevCommand(develop):
+    """Post-develop."""
+    def run(self):
+        develop.run(self)
+
+setuptools.setup(
+    name="edk2-pytool-base",
+    author="Tianocore Edk2-PyTool-Base team",
+    author_email="macarl@microsoft.com",
+    description="Python basetools supporting UEFI EDK2 firmware development",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/tianocore/edk2-pytool-base",
+    license='BSD-2-Clause-Patent',
+    packages=setuptools.find_packages(),
+    cmdclass={
+        'sdist': PostSdistCommand,
+        'install': PostInstallCommand,
+        'develop': PostDevCommand,
+    },
+    include_package_data=True,
+    use_scm_version=True,
+    setup_requires=['setuptools_scm'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers"
+    ]
+)
